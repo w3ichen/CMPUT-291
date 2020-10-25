f0a562ba6caedcc8f48d61b88a2b7de384c65bb1
weichen <weichen@github.com>
2020-10-24 19:08:42 -0600

    done for today

--- Mini Project 1/main.py
+++ Mini Project 1/main.py
@@ -3,6 +3,7 @@ from datetime import date
 import hashlib
 from User import User
 from Privileged import Privileged
+from getpass import getpass
 
 # cusor and connection global variables
 c = None
@@ -22,24 +23,23 @@ def start():
         # registered user
         print("\n----Login----")
         uid = input("User ID: ")
-        password = input("Password: ")
+        password = getpass()
         c.execute('SELECT * FROM users WHERE uid = :uid AND pwd = :pw;',
              { 'uid': uid, 'pw': hash_password(password) })
         user = c.fetchone()
         if (user == None):
             print("\nInvalid User ID or Password\n")
-            start()
+            return start()
         else:
             # check if user is privileged
             c.execute('SELECT * FROM privileged WHERE uid=:uid',{'uid':uid})
             if (c.fetchone() == None):
                 # not a privileged user
-                return User(uid, user["name"])
+                return User(uid, user[1])
             else:
                 # is a privileged user
                 print("Access: Privileged")
-                return Privileged(uid, user["name"])
-
+                return Privileged(uid, user[1])
 
     elif option == "2":
         # unregistered user
@@ -47,22 +47,23 @@ def start():
         uid = input("Unique ID: ")
         name = input("Name: ")
         city = input("City: ")
-        password = hash_password(input("Password: "))
+        password = hash_password(getpass())
         # check that user does not exist
         c.execute('SELECT * FROM users WHERE uid=:uid;',{'uid':uid})
         if (c.fetchone() == None):
             c.execute('''INSERT INTO users(uid,name,city,pwd,crdate) 
                         VALUES(:uid, :name, :city, :pwd, :crdate);''',
                 { 'uid':uid, 'name':name, 'city':city, 'pwd':password, 'crdate':date.today() })
+            conn.commit()
             print("Successfully Created",name,"User")
         else:
             print("\nUser Already Exists\n")
-            start()
+            return start()
         # return user object
         return User(uid, name)
     else:
         print("\nInvalid Option - Enter 1 or 2\n")
-        start()
+        return start()
 
 
 if __name__ == "__main__":
@@ -73,10 +74,10 @@ if __name__ == "__main__":
     c = conn.cursor()
 
     user = start()
+    
+    user.menu()
+
     # c.execute(''' ''')
-
-
-
     # c.execute("SELECT * FROM movie WHERE movie_number=:num and year=:year",
     # {"num":movie_number, "year": movie_year} )
     # conn.commit()
