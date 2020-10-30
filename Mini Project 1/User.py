698e726ab194e49a5e26bf6605ba6f6ec28a05f9
weichen <weichen@github.com>
2020-10-30 16:08:10 -0600

    added post seach answer functions

--- Mini Project 1/User.py
+++ Mini Project 1/User.py
@@ -58,16 +58,125 @@ class User():
             return self.menu()
 
     def post(self):
-        print("post a question")
+        print("\n -----Post a Question-----")
+        self.c.execute('SELECT COALESCE(MAX(pid),0) FROM posts;')
+        pid = int(self.c.fetchone()[0])+1
+        title = input("Please enter a title: ")
+        body = input("Please enter the body of your post: ")
+        poster_id = self.uid
+
+        self.c.execute('''INSERT INTO posts(pid,pdate,title,body,poster) 
+        VALUES(:pid, :pdate, :title, :body, :poster);''',
+        { 'pid':pid, 'pdate':date.today(), 'title':title, 'body':body, 'poster':poster_id })
+        
+        self.c.execute('''INSERT INTO questions(pid) 
+        VALUES(:pid);''',
+        { 'pid':pid })
+        
+        self.conn.commit()
+        print("Your question has been posted")
+        self.menu()
 
     def search(self):
-        print("search for posts")
-        # NEED TO UPDATE self.pid and self.isQuestion!!
-        # open menu for action on post
-        self.postActionMenu()
+        print("\n -----Search for posts-----")
+        entry = input("Please enter a keyword! \n")
+        entry.lower()
+        keywords = entry.split(' ')
+        keywords[:] = [item for item in keywords if item != '']
+
+        find_matches = '''SELECT  c.Type, p.pid, p.pdate, p.title, p.body, p.poster, COALESCE(A.Num_of_Answers,0) AS Num_of_Answers, COALESCE(B.Total_Votes,0) AS Total_Votes
+                        FROM posts p
+                        LEFT OUTER JOIN
+                        (SELECT  'Answer' AS Type, a.pid
+                        FROM answers a 
+                        UNION
+                        SELECT  'Question' AS Type, q.pid
+                        FROM questions q) as C on C.pid = p.pid
+                        LEFT OUTER JOIN
+                        (SELECT  p1.pid, COALESCE(COUNT(a1.qid),0) AS Num_of_Answers
+                        FROM posts p1, answers a1
+                        WHERE p1.pid = a1.qid
+                        GROUP BY (p1.pid)
+                        ) as A on A.pid = p.pid
+                        LEFT OUTER JOIN
+                        (SELECT  p2.pid,COUNT(v2.pid) AS Total_Votes
+                        FROM posts p2, votes v2
+                        WHERE p2.pid = v2.pid
+                        GROUP BY (p2.pid)
+                        ) as B on B.pid = p.pid
+                        LEFT OUTER JOIN
+                        (SELECT p3.pid AS test
+                        FROM posts p3, tags t
+                        WHERE p3.pid = t.pid
+                        AND lower(t.tag) like '%{}%') as C on C.test = p.pid
+                        WHERE 
+                        (lower(p.title) like '%{}%' or lower(p.body) like '%{}%' )
+                        GROUP BY p.pid
+                        ORDER BY COUNT(p.pid) DESC
+                    '''.format(keywords[0],keywords[0],keywords[0])
+        ## NOT SURE HOW TO SEARCH MULTIPLE KEYWORDS
+        self.c.execute(find_matches)
+        
+        rows = self.c.fetchall()
+                
+        output_array = []
+        output_array = [list(i) for i in rows]
+        
+        if  len(output_array) == 0:
+            print('\n',"Sorry! There are no matches found\n")
+            self.menu()
+                
+        for i in range(len(output_array)):
+            if i != 0 and i%5 == 0 :
+                inp_1 = input("\nEither select a post number or enter 'x' to see more posts\n")
+                inp_1.lower()
+                if (inp_1 == "x"):
+                    print('\n',output_array[i],'\n')
+                
+                elif(inp_1.isdigit()):
+                    print("User has selected post #",inp_1,"\n")
+                    self.pid = int(inp_1)
+                    ##THIS DOESNT WORK -- need new way of finding the post number in output array 
+                    if output_array[int(inp_1)][0] == "Question":
+                        self.isQuestion = True
+                    else:
+                        self.isQuestion = False
+                    self.postActionMenu()
+            else:
+                print('\n',output_array[i])
+                
+        inp_1 = input("Please select a post number\n")
+        inp_1.lower()
+        if (inp_1.isdigit()):
+            print("User has selected post #",inp_1,"\n")
+            self.pid = int(inp_1)
+            ##THIS DOESNT WORK -- need new way of finding the post number in output array 
+            if output_array[int(inp_1)][0] == "Question":
+                self.isQuestion = True
+            else:
+                self.isQuestion = False
+            self.postActionMenu()
 
     def answer(self):
-        print("answer question")
+        print("\n -----Post an Answer-----")
+        qid = self.pid
+        self.c.execute('SELECT COALESCE(MAX(pid),0) FROM posts;')
+        aid = int(self.c.fetchone()[0])+1
+        title = input("Please enter a title: ")
+        body = input("Please enter the body of your post: ")
+        poster_id = self.uid
+
+        self.c.execute('''INSERT INTO posts(pid,pdate,title,body,poster) 
+        VALUES(:aid, :pdate, :title, :body, :poster);''',
+        { 'aid':aid, 'pdate':date.today(), 'title':title, 'body':body, 'poster':poster_id })
+
+        self.c.execute('''INSERT INTO answers(pid,qid) 
+        VALUES(:aid, :qid);''',
+        { 'aid':aid, 'qid':qid })
+            
+        self.conn.commit()
+        print("Your answer has been recorded")
+        self.menu()
 
     '''
         Post action-Vote.The user should be able to vote on the post (if not voted already on the same post). 
